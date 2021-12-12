d11468f2834641d9d4413246bd6da0fc307712cf
gursimran18 <kaurgursimran9918@gmail.com>
2021-12-12 19:01:26 +0530

    defined database constants and user model

---
+++ CodeDocs-Server/src/main/java/models/User.java
@@ -0,0 +1,77 @@
+package models;
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String isVerified;
+    private String userID;
+
+    public User() {
+    }
+
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User(String userID, String email, String firstName, String lastName, String isVerified) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.isVerified = isVerified;
+        this.userID = userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIsVerified() {
+        return isVerified;
+    }
+
+    public void setIsVerified(String isVerified) {
+        this.isVerified = isVerified;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+}
