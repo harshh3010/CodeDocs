97a15eab9a057e279214add640bc5f1b14ab9bc3
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2021-12-12 17:32:32 +0530

    Initial commit

---
+++ CodeDocs-Client/src/main/java/CodeDocsClient.java
@@ -0,0 +1,5 @@
+public class CodeDocsClient {
+    public static void main(String[] args) {
+        System.out.println("Hello from client!");
+    }
+}
---
+++ CodeDocs-Server/src/main/java/CodeDocsServer.java
@@ -0,0 +1,5 @@
+public class CodeDocsServer {
+    public static void main(String[] args) {
+        System.out.println("Hello from Server!");
+    }
+}
