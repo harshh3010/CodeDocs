e1c9d22c5ef09982c8ed62bb5c9dd2e7a5eebde8
gursimran18 <kaurgursimran9918@gmail.com>
2021-12-14 23:02:35 +0530

    Invite collaborator service added

--- CodeDocs-Client/src/main/java/mainClasses/CodeDocsClient.java
+++ CodeDocs-Client/src/main/java/mainClasses/CodeDocsClient.java
@@ -51,40 +51,12 @@ public class CodeDocsClient extends Application {
             socket = new Socket(hostname, port);
             outputStream = new ObjectOutputStream(socket.getOutputStream());
             inputStream = new ObjectInputStream(socket.getInputStream());
-            /**InviteCollaboratorRequest request = new InviteCollaboratorRequest();
-            request.setCodeDocID("8556a73d-c261-4519-b9af-265a8b9c7327");
-            request.setReceiverID("954cb74b-2f4b-4431-8d14-42d86e639b73");
-            request.setSenderID("dc6d9b08-b53d-4199-88c8-5b64c40290c0");
-            request.setWritePermissions(1);
-            outputStream.writeObject(request);
-            outputStream.flush();
 
-            InviteCollaboratorResponse response = (InviteCollaboratorResponse) inputStream.readObject();
-            System.out.println(response.getStatus());
-            FetchInviteRequest fetchInviteRequest = new FetchInviteRequest();
-            fetchInviteRequest.setUserID("f93993c8-7dc1-4710-b204-291a6efb5219");
-            fetchInviteRequest.setOffset(1);
-            fetchInviteRequest.setRowcount(10);
-            outputStream.writeObject(fetchInviteRequest);
-             FetchInviteResponse response = (FetchInviteResponse) inputStream.readObject();
-             for( CodeDoc i : response.getInvites()) {
-             System.out.println(i.getTitle()+"  "+i.getOwnerName());
-             }
-             */
-
-            RunCodeDocRequest runCodeDocRequest = new RunCodeDocRequest();
-            runCodeDocRequest.setCodeDocID("ab49db3b-1584-4bba-8f52-4678a2211706");
-            runCodeDocRequest.setLanguageType(LanguageType.JAVA);
-            runCodeDocRequest.setUserID("dc6d9b08-b53d-4199-88c8-5b64c40290c0");
-            outputStream.writeObject(runCodeDocRequest);
-            outputStream.flush();
-            RunCodeDocResponse runCodeDocResponse = (RunCodeDocResponse) inputStream.readObject();
-            System.out.println(runCodeDocResponse.getError()+" *********\n"+runCodeDocResponse.getOutput());
 
             //inputStream.close();
             //outputStream.close();
             //socket.close();
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException  e) {
             e.printStackTrace();
         }
 
